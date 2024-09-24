fe4211a981a5f71b5b33a9eb98d3443b766e0bdd
getSemyon <122952539+getSemyon@users.noreply.github.com>
2024-09-24 21:16:00 +0700

    Create castom button
    Create test button

---
+++ CustomButton/CustomButton/UserButton.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomButton
+{
+    /// <summary>
+    /// Логика взаимодействия для UserButton.xaml
+    /// </summary>
+    public partial class UserButton : UserControl
+    {
+        public UserButton()
+        {
+            InitializeComponent();
+
+            /*BackgroundButton = new SolidColorBrush(Colors.White);
+            ForegroundButton = new SolidColorBrush(Colors.Black);
+            TextButton = "Text";*/
+        }
+
+        /*private Brush backgroundButton;
+        public Brush BackgroundButton
+        {
+            get { return backgroundButton; }
+
+            set {
+                backgroundButton = value; 
+                border.Background = backgroundButton;
+            }
+        }
+
+        private Brush foregroundButton;
+        public Brush ForegroundButton
+        {
+            get { return foregroundButton; }
+
+            set
+            {
+                foregroundButton = value;
+                text.Foreground = foregroundButton;
+            }
+        }
+
+        private string textButton;
+        public string TextButton
+        {
+            get { return textButton; }
+
+            set
+            {
+                textButton = value;
+                text.Text = textButton;
+            }
+        }*/
+    }
+}
